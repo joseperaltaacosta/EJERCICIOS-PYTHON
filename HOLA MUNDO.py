2c31c0068f9cfacc65062913760ad834108b0bcb
Jose <al_jperaltaa@iessierradelasvillas.org>
2022-09-29 10:02:56 +0200

    Comentarios, entrada y salida de datos y tipos de datos en python

---
+++ HOLA MUNDO.py
@@ -0,0 +1,17 @@
+
+
+nombre = "Jose"
+edad = 17
+mayor_o_menor_edad = False
+
+
+#Entrada y salida de datos
+#Salida con print()
+
+print("Buenos dias", nombre , "tu edad es", edad)
+
+#Entrada de datos == input()
+nombre = input("Dime tu nombre:\n") #\n permite hacer un salto de linea
+edad = input("Dime tu edad:\n")
+
+print("Buenos dias", nombre , "tu edad es", edad)
