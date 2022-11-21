a59d53088abcb3ed5f33505a2b44b11ae06c48fc
Jose <al_jperaltaa@iessierradelasvillas.org>
2022-11-21 11:14:40 +0100

    .set y .get

--- APUNTES/TKINTER.py
+++ APUNTES/TKINTER.py
@@ -1,21 +1,34 @@
 from tkinter import *
 from tkinter import ttk
+def saludar():
+    texto = campoTexto.get()
+    textoLabel.set(texto)
+
 #Generar la ventana
 ventana = Tk()
 ventana.config(background="aquamarine")
 ventana.title("TU_PAPI_XHULO")
-ventana.geometry("300x500")
+ventana.geometry("300x400")
 ventana.resizable(width=False,height=False)
-imagen=PhotoImage(file=("índice.png"))
-fondo=Label(ventana,image=imagen).place(x=10,y=20)
+imagen=PhotoImage(file="foto4.png")
+fondo=Label(ventana,image=imagen).place(x=20,y=120)
 
 #Genera el lienzo para pintar componentes
 frm = ttk.Frame(ventana).pack()
 
 #Componentes label y button
-labelTexto=ttk.Label(frm, text="hello papi :)")
-labelTexto.config(background="light blue", border=5, font=("Arial",15))
+textoLabel=StringVar()
+textoLabel.set("iSHOWSPEED")
+labelTexto=ttk.Label(frm, textvariable=textoLabel)
+labelTexto.config(background="light blue", border=5, font=("courier",30))
 labelTexto.pack()
+
+#Componente cuadro de texto
+campoTexto=ttk.Entry(frm)
+campoTexto.pack()
+
+ttk.Button(frm, text="Saludo", command=saludar).pack()
+
 ttk.Button(frm, text="Salir", command=ventana.destroy).pack()
 
 ventana.mainloop()
